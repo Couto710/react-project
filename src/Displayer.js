8fea75b4e9fdbdf5632e6a32488e68c6ab4a531a
Couto710 <pedro.jope.couto@gmail.com>
2018-11-16 19:15:09 +0000

    table created and styled, logic next

--- src/Displayer.js
+++ src/Displayer.js
@@ -3,14 +3,16 @@ import './styles/Displayer.css';
 
 export class Displayer extends React.Component {
 
+
+
     render() {
 
         return (
             <table id="table">
-                <tr>
-                    <th>name</th>
-                    <th>Country</th>
-                    <th>birthday</th>    
+                <tr id ="headrow">
+                    <th id="r1">name</th>
+                    <th id="r2">Country</th>
+                    <th id="r3">birthday</th>    
                 </tr>            
             </table>
         )
